b4fb1057ad9741934326b6af1a8bfb14238ae722
yanhappiness <457029997@qq.com>
2017-06-20 19:37:25 +0800

    swiper插件版本

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 
 $(document).ready(function () {
+
+	/* 定位到当前类 */
+	function index(num){
+		$('.menu li').eq(num).addClass('active').siblings().removeClass('active');
+	}
+	$('.menu li').click(function(){
+		$('.menu li').removeClass("active");
+		$(this).addClass("active");
+	})
+
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
-        paginationClickable: true
-    });
+        paginationClickable: true,
+        onSlideChangeStart: function(swiper){
+      		index(swiper.activeIndex);
+      	}
+	});
 
-    var mySwiper = new Swiper('.swiper-container',{
-		onSlideChangeStart: function(swiper){
-      		alert(swiper.activeIndex);
-    	}
-    })
+    var aa= document.querySelectorAll('nav .nav a');
+	for(var i=0;i<aa.length;i++){
+		(function(i){
+			aa[i].onclick = function(){
+				swiper.slideTo(i, 1000, false);//切换到第一个slide，速度为1秒
+			}
+		})(i);
+	}
 })
